29077196c8c4277670faf6bcf150b847bd350936
Vincent laurensius <vincenthambaya@gmail.com>
2023-09-11 12:46:08 +0700

    [FEAT] Enable config for next MF in auth app

--- packages/auth-app/next.config.js
+++ packages/auth-app/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+module.exports = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'authApp',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './ButtonAuth': './components/ButtonDemo.tsx',
+        },
+        shared: {
+          // whatever else
+        },
+      })
+    );
+
+    return config;
+  },
+};
